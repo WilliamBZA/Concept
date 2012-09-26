15fdbf2422ed4874b5f15ae19d2ad0a0faf7189f
William Brander <william@williamb.net>
2012-09-26 07:21:45 +0200

    Initial commit

---
+++ Concept/Concept/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Concept.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "This will become the new Concept Cleaning website";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+    }
+}
---
+++ Concept/Concept/ViewModels/OnlineEnquiryViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Concept.ViewModels
+{
+    public class OnlineEnquiryViewModel
+    {
+        public string Name { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Message { get; set; }
+    }
+}
